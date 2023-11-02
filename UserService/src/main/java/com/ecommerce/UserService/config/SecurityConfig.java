b87c88c118493742ab978691444602ef6e96cdcd
SIMONE\simon <simonecampisi2003@gmail.com>
2023-11-02 17:58:19 +0100

    add OAuth2 in UserService

---
+++ UserService/src/main/java/com/ecommerce/UserService/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.ecommerce.UserService.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { //Aggiungere chiamata di callback a un'API nel controller in seguito al login.
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+        return http.build();
+    }
+}
