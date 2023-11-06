02f35a1488ab342ec13a99010de3dc5ea747057f
SIMONE\simon <simonecampisi2003@gmail.com>
2023-11-06 18:02:24 +0100

    finish implements Security in Gateway. Must test.

---
+++ gateway/src/main/java/com/ecommerce/gateway/filter/AuthFilter.java
@@ -0,0 +1,54 @@
+package com.ecommerce.gateway.filter;
+
+import com.ecommerce.gateway.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private RouteValidator validator;
+
+    @Autowired
+    private RestTemplate template;
+
+    public AuthFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (((exchange, chain) -> {
+            if(validator.isSecured.test(exchange.getRequest())) {
+                if(!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    throw new RuntimeException("JWT mancante nell'header.");
+                }
+
+                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0); //si prende il token
+                if(authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                }
+
+                try {
+//                    template.getForObject("http://localhost:8084/users/auth/validate?token="+authHeader, String.class); //bisogna criptarla
+                    jwtUtil.validateToken(authHeader);
+                } catch (Exception e) {
+                    throw new RuntimeException("JWT non valida.");
+                }
+            }
+
+            return chain.filter(exchange);
+        }));
+    }
+
+    public static class Config {
+
+    }
+}
