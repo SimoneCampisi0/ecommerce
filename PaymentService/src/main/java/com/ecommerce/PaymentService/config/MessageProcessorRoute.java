5d000348ed92951f51d203d398820d443327d543
SIMONE\simon <simonecampisi2003@gmail.com>
2023-11-10 15:23:01 +0100

    add some request and response

--- PaymentService/src/main/java/com/ecommerce/PaymentService/config/MessageProcessorRoute.java
+++ PaymentService/src/main/java/com/ecommerce/PaymentService/config/MessageProcessorRoute.java
@@ -1,6 +1,6 @@
 package com.ecommerce.PaymentService.config;
 
-import com.ecommerce.PaymentService.dto.response.SendOrderResponse;
+import com.ecommerce.PaymentService.dto.response.CommonOrdersResponse;
 import com.ecommerce.PaymentService.service.PaymentService;
 import com.ecommerce.PaymentService.utils.ObjectMapperImpl;
 import org.apache.camel.builder.RouteBuilder;
@@ -18,9 +18,12 @@ public class MessageProcessorRoute extends RouteBuilder { //riceve il messaggio 
 
     @Override
     public void configure() {
-        from("activemq:ordini")
+        from("activemq:topayments")
                 .process(exchange -> {
-                    String jsonResponse = service.readAndSendOrder(objectMapperImpl.readValue(exchange.getIn().getBody(String.class), SendOrderResponse.class));
+                    String jsonResponse = service.readAndSendOrder(objectMapperImpl.readValue(exchange.getIn().getBody(String.class), CommonOrdersResponse.class));
+
+                    System.out.println("\n\n\n"+jsonResponse+"\n\n\n");
+
                     exchange.getMessage().setBody(jsonResponse);
                 })
                 .to("activemq:payments");
